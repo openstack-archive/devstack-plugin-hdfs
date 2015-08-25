834167a6a52d9ae27050ce07fb99d6b0295e0f09
Li, Chen <chen.li@intel.com>
2015-08-25 14:59:46 +0800

    Test HDFS
    Change-Id: I82a70d67209f27edead50d64dea03e5cb5ce925a

--- devstack/lib/hdfs-functions.sh
+++ devstack/lib/hdfs-functions.sh
@@ -7,13 +7,17 @@
 # Triggered from devstack/plugin.sh as part of devstack "install"
 function install_hdfs {
     # install nessary packages
-    install_package openssh-server expect
+    install_package openssh-server
 
     # Set ssh with no password
-    if [[ ! -e $DEST/.ssh/id_rsa.pub ]]; then
-        ssh-keygen -q -N '' -t rsa -f  $DEST/.ssh/id_rsa
+    if [[ ! -e ~/.ssh/id_rsa.pub ]]; then
+        ssh-keygen -q -N '' -t rsa -f  ~/.ssh/id_rsa
     fi
-    cat  $DEST/.ssh/id_rsa.pub >> $DEST/.ssh/authorized_keys
+    cat  ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+    ssh-keygen -R 127.0.0.1
+    $HDFS_PLUGIN_LIB_DIR/autossh.sh "ssh-copy-id -i 127.0.0.1"
+    ssh-keygen -R 0.0.0.0
+    $HDFS_PLUGIN_LIB_DIR/autossh.sh "ssh-copy-id -i 0.0.0.0"
 
     if [[ -z $JAVA_HOME ]]; then
         install_package openjdk-7-jre openjdk-7-jdk
@@ -36,10 +40,10 @@ function configure_hdfs {
     # edit core-site.xml & hdfs-site.xml
     cp $HDFS_PLUGIN_LIB_DIR/template/* $HDFS_PLUGIN_HADOOP_DIR/etc/hadoop/
 
-    path=${HDFS_PLUGIN_DIR//\//@}
+    path=${HDFS_PLUGIN_DIR//\//__slashplaceholder__}
 
     sed -i "s/__PLACEHOLDER__/$path/g" $HDFS_PLUGIN_HADOOP_DIR/etc/hadoop/hdfs-site.xml
-    sed -i 's/@/\//g' $HDFS_PLUGIN_HADOOP_DIR/etc/hadoop/hdfs-site.xml
+    sed -i 's/__slashplaceholder__/\//g' $HDFS_PLUGIN_HADOOP_DIR/etc/hadoop/hdfs-site.xml
 
     # formate namenode
     $HDFS_PLUGIN_HADOOP_DIR/bin/hdfs namenode -format
@@ -48,10 +52,26 @@ function configure_hdfs {
 # start_hdfs() - Start running processes
 function start_hdfs {
     # start
-    $HDFS_PLUGIN_LIB_DIR/start_hdfs.sh $HDFS_PLUGIN_HADOOP_DIR/sbin/start-dfs.sh
+    $HDFS_PLUGIN_HADOOP_DIR/sbin/start-dfs.sh
+
     # add hadoop/bin to PATH
-    echo "export PATH=$PATH:$HDFS_PLUGIN_HADOOP_DIR/bin " >> ~/.bashrc
+    ori_path=$PATH:$HDFS_PLUGIN_HADOOP_DIR/bin
+    no_slash_path=${ori_path//\//__slashplaceholder__}
+    new_path=${no_slash_path//\:/__colonplaceholder__}
+    sed -i "1 s/^/PATH=${new_path}\n/" ~/.bashrc
+    sed -i 's/__slashplaceholder__/\//g'  ~/.bashrc
+    sed -i 's/__colonplaceholder__/\:/g'  ~/.bashrc
     source ~/.bashrc
+}
+
+# test_hdfs() - Testing HDFS
+function test_hdfs {
+    cat ~/.bashrc
+    echo $PATH
+    source ~/.bashrc
+    echo $PATH
+
+    hdfs fsck /
 }
 
 # Stop running hdfs service
